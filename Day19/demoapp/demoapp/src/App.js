0f19aef0f129323010daeb1a529ae7af5effc288
mukund <autimukund9@gmail.com>
2025-11-14 11:26:47 +0530

    WPT

--- Day19/demoapp/demoapp/src/App.js
+++ Day19/demoapp/demoapp/src/App.js
@@ -1,50 +1,43 @@
 import react from "react";
 import { BrowserRouter,Routes,Route,NavLink,Link,useNavigate } from "react-router-dom";
 import Login from './Login';
-import About from './About';
-import Home from './Home';
-import Contact from './Contact';
-import AppContext from "./Assignment2";
+// import About from './About';
+// import Home from './Home';
+// import Contact from './Contact';
+// import AppContext from "./Assignment2";
 
  const App1=()=>{
 return (
-// <BrowserRouter>
-
-// <nav style={{backgroundColor:"red",justifyContent: "center",}}>
-// </nav>
-// <ul>
-//   <li>
-//      <NavLink to="/" >Home</NavLink>
-//   </li>
-//   <li>
-// <Link to="/about" >About Us</Link>
-
-//   </li>
-//   <li>
-// <Link to="/contact" >Contact Us</Link>
-//   </li>
-// <li>
-// <Link to="/login" >Login</Link>
-// </li>
-// </ul>
-// <div>
-// <Routes>
-//   <Route path ="/" element={<Home/>}></Route>
-//   <Route path ="/about" element={<About/>}></Route>
-//   <Route path ="/contact" element={<Contact/>}></Route>
-//   <Route path ="/login" element={<Login/>}></Route>
-//   </Routes>
 
 
+<Login></Login>
 
-// </div>
-// </BrowserRouter>
-<AppContext></AppContext>
 
 
 )
  }
 export default App1;
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Home from './Home';
+// import AboutUs from './Aboutus';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//      <Home></Home>
+//      <AboutUs></AboutUs>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
